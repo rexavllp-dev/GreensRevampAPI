81b1423ccffd0cf040cf0fbd42ce56d775b5adbf
ananthu-rexav <114664906+ananthu-rexav@users.noreply.github.com>
2023-12-27 00:38:11 +0530

    refresh token route change and verify token middleware added

---
+++ backend/api/middleware/verifyToken.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const verifyToken = (req, res, next) => {
+    const token = req.header("Authorization")?.split(" ")[1]
+
+    if (!token) {
+        res.status(401).json({ status: 401, auth: false, message: "Authentication failed. Token not found" });
+    }
+
+    else {
+        jwt.verify(token, process.env.JWT_ACCESS, (err, decoded) => {
+            if (err) {
+                console.log(err)
+                res.status(401).json({ status: 401, auth: false, message: "Invalid token" });
+            }
+            else {
+                req.user = decoded;
+                next();
+            }
+        })
+    }
+}
+
+export default verifyToken;
