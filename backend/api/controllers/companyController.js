a55dcb98d13c4d4e6aee1b1a34db4615e8940263
abhijithp327 <153506589+Abhijith327@users.noreply.github.com>
2024-01-02 15:59:55 +0530

    added emailer update

--- backend/api/controllers/companyController.js
+++ backend/api/controllers/companyController.js
@@ -253,7 +253,7 @@ export const registerCompany = async (req, res) => {
         const token = jwt.sign({ userId, usr_email, usr_firstname, usr_company }, process.env.EMAIL_SECRET, { expiresIn: "600s" });
 
         // Send email verification link
-        await sendVerificationEmail(usr_email, token, 'company');
+        await sendVerificationEmail(usr_email, usr_firstname, token, 'company');
 
 
 
