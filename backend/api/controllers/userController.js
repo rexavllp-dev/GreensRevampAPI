0fd6884bfde5ccba1f7c724cadcb7cab4c459e9a
anuragRexav <153506994+anuragRexav@users.noreply.github.com>
2024-01-09 15:54:31 +0530

    company  message changed

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -438,7 +438,7 @@ export const loginWithOtp = async (req, res) => {
                 return res.status(403).json({
                     status: 403,
                     success: false,
-                    message: 'Company is not verified. Please contact admin for assistance.',
+                    message: 'your account activation is under process we will update once your account is active.',
                 });
             }
 
