9f5e2895a4f22722eb27d7fd2855f4d3dd08ef6a
anuragRexav <153506994+anuragRexav@users.noreply.github.com>
2024-01-23 13:31:27 +0530

    stock history get all  added

---
+++ backend/api/controllers/stockHistory.js
@@ -0,0 +1,28 @@
+// get all stock history
+
+import { getAllStockHistory } from "../models/stockHistoryModel";
+
+export const getStockHistory = async (req, res) => {
+
+    try {
+
+    const stockHistory = await getAllStockHistory();
+    res.status(200).json({
+        status: 200,
+        success: true,
+        message: "Stock History fetched successfully",
+        result: stockHistory
+    })
+
+    } catch (error) {
+
+    res.status(500).json({
+        status: 500,
+        success: false,
+        message: "Failed to fetch stock history",
+        error: error
+    })
+    }
+}
+
+
--- backend/api/models/stockHistoryModel.js
+++ backend/api/models/stockHistoryModel.js
@@ -1,2 +1,11 @@
+import db from '../../config/dbConfig.js';
 
+// get all stock history 
+
+
+export const getAllStockHistory = async () => {
+
+    const stockHistory = await db('stock_history').select('*');
+    return stockHistory;
+}
 
