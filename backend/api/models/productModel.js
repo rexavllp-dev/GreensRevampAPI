67fb3b8976ba25d60a1c47ed785120815d4255e4
abhijithp327 <153506589+Abhijith327@users.noreply.github.com>
2024-02-05 11:34:38 +0530

    update in search

--- backend/api/models/productModel.js
+++ backend/api/models/productModel.js
@@ -165,7 +165,7 @@ export const getAllProducts = async (page, per_page, search, filters, sort) => {
     if (search) {
         console.log(search);
         query.where(function () {
-            this.whereRaw(`similarity(products.prd_name, ?) > 0.2`, [search]) // Search similarity in product name
+            this.whereRaw(`similarity(products.prd_name, ?) > 0.1`, [search]) // Search similarity in product name
                 .orWhereRaw(`similarity(product_inventory.sku, ?) > 0.2`, [search]); // Search similarity in SKU
         });
     };
@@ -437,7 +437,7 @@ export const fetchAllOptionProducts = async (page, per_page, search, filters, so
 
     if (search) {
         console.log(search);
-        query.whereRaw(`similarity(products.prd_name, ?) > 0.2`, [search]);
+        query.whereRaw(`similarity(products.prd_name, ?) > 0.1`, [search]);
     };
 
     // Apply complex filters
