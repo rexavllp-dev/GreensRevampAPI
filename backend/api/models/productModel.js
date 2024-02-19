51a8c25c7121d4f35083028fe3d4f9d8076cf195
abhijithp327 <153506589+Abhijith327@users.noreply.github.com>
2024-02-19 16:39:06 +0530

    update sorting

--- backend/api/models/productModel.js
+++ backend/api/models/productModel.js
@@ -282,40 +282,34 @@ export const getAllProducts = async (page, per_page, search, filters, sort, minP
     } else if (sort === 'oldest') {
         query.orderBy('products.created_at', 'asc'); // Assuming 'created_at' is the timestamp field for product creation
     } else if (sort === 'bestsellers') {
-        query.orderBy('product_inventory.best_seller', 'desc'); // Assuming best_seller is a boolean field
+        query.orderBy('product_inventory.best_seller', 'asc'); // Assuming best_seller is a boolean field
     }
 
 
 
 
+
+
+    // Apply availability filters
     // Apply availability filters
     filters.forEach(filter => {
         if (filter.column === 'product_inventory.stock_availability') {
-            // Handle stock availability filter
             if (filter.value === 'In stock') {
                 query.where(function () {
-                    // Product is in stock if stock_availability is 'In stock'
-                    this.where('product_inventory.stock_availability', '=', 'In stock');
-                }).andWhere(function () {
-                    // Or, if inventory management is true and product quantity is greater than 0
-                    this.where('product_inventory.inventory_management', true)
-                        .andWhere('product_inventory.product_quantity', '>', 0)
-                }).orWhere(function () {  
-                    // Or, if inventory management is false
-                    this.where('product_inventory.inventory_management', false);
-                })
+                    this.where('product_inventory.stock_availability', '=', 'In stock')
+                        .orWhere(function () {
+                            this.where('product_inventory.inventory_management', true)
+                                .andWhere('product_inventory.product_quantity', '>', 0);
+                        });
+                });
             } else if (filter.value === 'Out of stock') {
                 query.where(function () {
-                    // Product is out of stock if stock_availability is 'Out of stock'
-                    this.where('product_inventory.stock_availability', '=', 'Out of stock');
-                }).orWhere(function () {
-                    // Or, if inventory management is true and product quantity is 0
-                    this.where('product_inventory.inventory_management', true)
-                        .andWhere('product_inventory.product_quantity', '=', 0);
+                    this.where('product_inventory.stock_availability', '=', 'Out of stock')
+                        .orWhere(function () {
+                            this.where('product_inventory.inventory_management', true)
+                                .andWhere('product_inventory.product_quantity', '=', 0);
+                        });
                 });
-
-
-                
             }
         } else {
             // Handle other types of filters (if any) here
@@ -328,6 +322,8 @@ export const getAllProducts = async (page, per_page, search, filters, sort, minP
             }
         }
     });
+
+
 
 
 
