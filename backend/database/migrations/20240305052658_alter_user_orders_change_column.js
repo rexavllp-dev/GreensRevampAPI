c702a1798fb2b5cbf257adccfd208b448672378d
ananthu-rexav <114664906+ananthu-rexav@users.noreply.github.com>
2024-03-05 11:16:00 +0530

    migration added

---
+++ backend/database/migrations/20240305052658_alter_user_orders_change_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async (knex) => {
+    await knex.schema.alterTable('user_orders', (table) => {
+        table.integer('ord_qc_confirmed').unsigned().references('id').inTable('users').alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async (knex) => {
+    return knex.schema.alterTable('user_orders', (table) => {
+        table.boolean('ord_qc_confirmed').alter();
+    });
+};
