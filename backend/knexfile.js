7952d6954d09308ec7634f905a8fb0098d90c13f
Rajath-Rexav <rexavllp@gmail.com>
2024-03-23 22:15:16 +0530

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -10,7 +10,7 @@ export default {
       host: '127.0.0.1',
       database: 'greensinternational',
       user:     'postgres',
-      password: '12345',
+      password: 'postgres',
     }
   },
   // development: {
